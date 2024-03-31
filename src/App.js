2acd6295892e72bc64b702758969937da0ccd35a
pandapls <932650719@qq.com>
2024-03-31 21:58:54 +0800

    feat: 更新测试文案

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello, cloudflare pages
+          hello, cloudflare pages test2
         </p>
       </header>
     </div>
