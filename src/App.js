6857fc393e58e259b40fcb218576607b4eef87bc
pandapls <64623536+pandapls@users.noreply.github.com>
2024-04-06 09:24:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react"
 
 function App() {
+    const [rdata, setData] = useState();
+    useEffect(() => {
+      fetch('https://example.lhf932650719.workers.dev/')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setData(data)
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+  
+    }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           hello, cloudflare pages test2
+          {rdata}
         </p>
       </header>
     </div>
